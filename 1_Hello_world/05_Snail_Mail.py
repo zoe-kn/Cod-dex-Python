06b19857506d224e9c1c4432dd02540f39da7db8
zoe <zoe2002klein@gmail.com>
2025-07-06 21:22:16 +0200

    Create 05_Snail_Mail.py

---
+++ 1_Hello_world/05_Snail_Mail.py
@@ -0,0 +1,5 @@
+print('06. July 2025') #Today's date
+print('I am feeling good today.') #How am I feeling
+print('I wanna get a jab in computer science.') #What I want to accomplish by learning to code
+print('I am very proud of you, that you got your computer science degree!') # A message to my older, wiser and programmer self
+print('🦦🦦🦦🦦🦦🦦🦦') # My favorite emoji to spice things up
