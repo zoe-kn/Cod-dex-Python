f3220da87b1c03688ab9fa1156dd5c57cdc22adf
zoe <zoe2002klein@gmail.com>
2025-07-06 23:17:11 +0200

    Create 09_extra_quadratic_formula.py

---
+++ 2_Variables/09_extra_quadratic_formula.py
@@ -0,0 +1,10 @@
+a = int(input("Enter a: "))
+b = int(input("Enter b: "))
+c = int(input("Enter c: "))
+
+
+x1 = (-b+(b**2 -4*a*c)**0.5)/2*a)
+x2 = (-b-(b**2 -4*a*c)**0.5)/2*a)
+
+print(x1)
+print(x2)
