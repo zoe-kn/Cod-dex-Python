538d16d694946d97a7451a6a87804672ef717bd7
zoe <zoe2002klein@gmail.com>
2025-07-07 23:03:38 +0200

    Create 19_detemtion.py

---
+++ 4_Loops/19_detemtion.py
@@ -0,0 +1,5 @@
+# Task
+# Suppose you got detention and the teacher wants you to write "I will not use Snapchat in class" on the whiteboard 100 times.
+
+for count in range(100):
+  print('I will not use Snapchat in class')
