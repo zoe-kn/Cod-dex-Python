7362556efeb294b879c18e34f8f207e29cf457e7
zoe <zoe2002klein@gmail.com>
2025-07-07 22:19:10 +0200

    Update 18_guess_number.py

--- 4_Loops/18_guess_number.py
+++ 4_Loops/18_guess_number.py
@@ -1,6 +1,15 @@
+#Task 
+#First, introduce a variable called tries at the top and give it a value of 0.
+#Then, add a second condition with the tries variable to the while loop using a relational operator.
+
+tries = 0
 guess = 0
 
-while guess != 6:
+while guess != 6 and tries < 5:
   guess = int(input("Guess the number:  "))
+  tries = tries + 1
 
-print("You got it!")
+if guess != 6:
+  print('you ran out of tries')
+else:
+  print("You got it!")
