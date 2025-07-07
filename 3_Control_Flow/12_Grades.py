73d1696c3ec7230c6a44766bce9a2af2f2725d6a
zoe <zoe2002klein@gmail.com>
2025-07-07 11:14:40 +0200

    Create 12_Grades.py

---
+++ 3_Control_Flow/12_Grades.py
@@ -0,0 +1,13 @@
+#Task
+#Create a grades.py program that checks whether a grade is above or below 55.
+#Start by creating a variable called grade and give it a value between 0-100.
+#Write a if/else statement for the following:
+#If grade is greater than or equal to 55, then print "You passed."
+#Else, print "You failed."
+
+grade = int(input('Grade: '))
+
+if grade >= 55:
+   print('You passed.')
+else:
+  print('You failed.')
