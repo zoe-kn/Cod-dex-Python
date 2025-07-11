e3ea185d4f3f99dffe82da6c781bf5987d106749
zoe <zoe2002klein@gmail.com>
2025-07-11 22:06:34 +0200

    Create 24_inventory.py

---
+++ 5_Lists/24_inventory.py
@@ -0,0 +1,13 @@
+# TAsk
+# Create an Legoparts List
+# Can you figure out the following information using built-in list functions?
+# Which LEGO part are the elves running low on? Use min() to find out.
+# Is there a LEGO part that the elves are overstocking? Use max() to find out.
+
+
+
+lego_parts = [8980, 7323, 5343, 82700, 92232, 1203, 7319, 8903, 2328, 1279, 679, 589]
+
+print(min(lego_parts))
+
+print(max(lego_parts))
