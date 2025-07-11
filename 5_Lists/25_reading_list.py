ead903ee7df12f4dc18177d238b390b109c78099
zoe <zoe2002klein@gmail.com>
2025-07-11 22:10:45 +0200

    Create 25_reading_list.py

---
+++ 5_Lists/25_reading_list.py
@@ -0,0 +1,17 @@
+# Task
+# Create a book list
+# add 'Patchinko'
+# delete 'The Fault in Our Stars'
+# remove '1984'
+
+
+books = ['Harry Potter', '1984', 'The Fault in Our Stars', 'The Mom Test', 'Life in Code'
+]
+
+books.append('Patchinko')
+
+books.remove('The Fault in Our Stars')
+
+books.pop(1)
+
+print(books)
