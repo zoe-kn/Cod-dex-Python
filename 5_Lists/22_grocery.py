9274a1c02964ff5d369230998ec330e5e72e3ee1
zoe <zoe2002klein@gmail.com>
2025-07-11 21:53:31 +0200

    Create 22_grocery.py

---
+++ 5_Lists/22_grocery.py
@@ -0,0 +1,4 @@
+#Task
+#Create a grocery.py program with a grocery list of things that you need from the store
+
+grocery = ['🥚Eggs', '🥑 Avocados', '🍪 Cookies', '🌶 Hot Pepper Jam', '🫐 Blueberries', '🥦 Broccoli']
