2d3432abc1d3c5b0229c1356326b0bad07e784d2
zoe <zoe2002klein@gmail.com>
2025-07-11 22:03:27 +0200

    Create 23_to-do.py

---
+++ 5_Lists/23_to-do.py
@@ -0,0 +1,15 @@
+#Task
+#Create a todo.py program that will define a todo list that contains the following items
+#Print the first item and the second item. Next, use slicing to print the third, fourth, and fifth items. Try printing out the item at index 9 to see the IndexError
+
+todo = ['🏦 Get quarters.', '🧺 Do laundry.', '🌳 Take a walk.', '💈 Get a haircut.', 
+'🍵 Make some tea.', '💻 Complete Lists chapter.', '💖 Call mom.', '📺 Watch My Hero Academia.']
+
+# Index: 0 1 2 3 4
+
+
+print(todo[0 : 2])
+
+print(todo[2 : 5])
+
+print(todo[9]) #Task see the IndexError
